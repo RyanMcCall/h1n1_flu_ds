6c55644c626e5df61c43d758ef8185408d1eb956
Ryan McCall <ryan.e.mccall@gmail.com>
2020-07-17 13:59:47 -0500

    Add a run placeholder

--- src/obtain.py
+++ src/obtain.py
@@ -1,17 +1,3 @@
-from os import path
-import pandas as pd
-
-def get_h1n1_features():
-    if path.isfile('../data/raw/h1n1_features.csv'):
-        return pd.read_csv('../data/raw/h1n1_features.csv')
-    else:
-        data = pd.read_csv('https://s3.amazonaws.com/drivendata-prod/data/66/public/training_set_features.csv')
-        pd.to_csv('../data/raw/h1n1_features.csv', index=False)
-        return data
-        
-def get_h1n1_labels():
-    if path.isfile('../data/raw/h1n1_labels.csv'):
-        return pd.read_csv('')
 
 def run():
-    "Obtain: downloading raw data files..."
+    print('Obtaining data...')
