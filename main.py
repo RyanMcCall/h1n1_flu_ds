d63914a8a37d48bf94fa49ad8afa22284412dae8
Ryan McCall <ryan.e.mccall@gmail.com>
2020-08-20 17:45:29 -0500

    Change scrub to prepare in import and run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import src.acquire
-import src.scrub
+import src.prepare
 import src.explore
 import src.model
 import src.interpret
@@ -7,7 +7,7 @@ import src.interpret
 # python main.py
 if __name__ == "__main__":
     src.acquire.run()
-    src.scrub.run()
+    src.prepare.run()
     src.explore.run()
     src.model.run()
     src.interpret.run()
